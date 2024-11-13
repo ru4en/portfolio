aa4ac4bf48682d255fbe16e14b66ccd37ed3cfa6
Ruben Lopes <ru4en@pm.me>
2024-11-13 05:16:16 +0000

    minor

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,10 +1,8 @@
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 import Navbar from "../components/Nav/Navbar";
 import Footer from "../components/Footer/Footer";
 import Home from "../components/Home/Home";
-
 import CV from "../components/CV/CV";
 import Admin from "../components/Admin/Admin";
 import AboutMe from "../components/About Me/AboutMe";
@@ -12,24 +10,23 @@ import Blog from "../components/Blog/Blog";
 import NotFound from "../components/NotFound";
 // import Bugger from "../lib/Bugger";
 
-
 function AppRouter() {
   return (
-    <Router>
+    <HashRouter> {/* Use HashRouter for GitHub Pages compatibility */}
       <div>
         {/* <Bugger /> */}
         <Navbar />
         <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about-me" element={<AboutMe />} />
-        <Route path="/cv" element={<CV />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about-me" element={<AboutMe />} />
+          <Route path="/cv" element={<CV />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
